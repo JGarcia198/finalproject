af065613e7273db23054d032c1332823e65398d8
Jameson.Garcia <jameson.garcia198@gmail.com>
2025-12-24 14:51:34 -0500

    fix(cors): allow Vercel frontend origin

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ const db = require("./queries");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(
-    cors({
-      origin: process.env.CORS_ORIGIN || "*",
-    })
-  );
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: false
+  }));
+  
   
 app.use(express.json());
 
